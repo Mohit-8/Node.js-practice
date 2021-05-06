556ded070e4f41c3e0cf7a8d1ad71b670a2fe0e7
Mohit <58105910+Mohit-8@users.noreply.github.com>
2021-05-06 20:30:36 +0530

    files uploaded

---
+++ http-example/http-demo.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const { readFileSync } = require("fs");
+
+//getting the files
+const page = readFileSync("./index.html");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url == "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(page);
+    res.end();
+  } else if (url == "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1> about page </h1>");
+    res.end();
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1> page not found </h1>");
+
+    res.end();
+  }
+});
+
+server.listen(5000);
