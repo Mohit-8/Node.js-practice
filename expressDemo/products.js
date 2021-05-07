94502ba08d8cb634edc7a191c7aca73b215a3a63
Mohit <58105910+Mohit-8@users.noreply.github.com>
2021-05-08 00:53:22 +0530

    JSON data

---
+++ expressDemo/products.js
@@ -0,0 +1,37 @@
+const products = [
+  {
+    id: 1,
+    name: 'albany sofa',
+    price: 39.95,
+    desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+  },
+  {
+    id: 2,
+    name: 'entertainment center',
+
+    price: 29.98,
+    desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+  },
+  {
+    id: 3,
+    name: 'albany sectional',
+
+    price: 10.99,
+    desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+  },
+  {
+    id: 4,
+    name: 'leather sofa',
+
+    price: 9.99,
+    desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+  },
+];
+const people = [
+  { id: 1, name: 'john' },
+  { id: 2, name: 'peter' },
+  { id: 3, name: 'susan' },
+  { id: 4, name: 'anna' },
+  { id: 5, name: 'emma' },
+];
+module.exports = { products, people };
