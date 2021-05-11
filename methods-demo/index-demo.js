051f9b49a4918215dec9d399800eb9256d233731
Mohit <58105910+Mohit-8@users.noreply.github.com>
2021-05-11 23:38:59 +0530

    methods using frontend

---
+++ methods-demo/index-demo.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('./public'));
+//parse form data
+app.use(express.urlencoded({ extended: false }));
+
+//parse json data
+app.use(express.json());
+
+let { people } = require('../products');
+
+app.get('/api/people', (req, res) => {
+  res.status(200).json({ success: true, data: people });
+});
+
+app.post('/api/people', (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: 'please provide name value' });
+  }
+  res.status(201).json({ success: true, person: name });
+});
+
+app.put('/api/people/:id', (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const person = people.find((person) => person.id === Number(id));
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${id}` });
+  }
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+  res.status(200).json({ success: true, data: newPeople });
+});
+
+app.delete('/api/people/:id', (req, res) => {
+  const person = people.find((person) => person.id === Number(req.params.id));
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${req.params.id}` });
+  }
+  const newPeople = people.filter(
+    (person) => person.id !== Number(req.params.id)
+  );
+  return res.status(200).json({ success: true, data: newPeople });
+});
+
+app.post('/login', (req, res) => {
+  //console.log(req.body);
+
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Hey ${name}`);
+  }
+  res.status(401).send('Please provide data');
+});
+
+app.listen(5000, () => {
+  console.log('server is listening on port 5000');
+});
